202f6a8b5dc10512e4f56f9acf61f4c9c54ded62
Iiris Wahriz <iiris.wahriz@student.careeria.fi>
2022-01-17 14:13:23 +0200

    git

---
+++ olioohjelmointi/olioohjelmointi/harjoitus10/harjoitus12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace harjoitus12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> pakka = new List<string>();
+
+            for (int i = 0; i < 13; i++)
+            {
+                pakka.Add("Pata - " + (i + 1));
+                pakka.Add("Hertta - " + (i + 1));
+                pakka.Add("Ruutu - " + (i + 1));
+                pakka.Add("RIsti - " + (i + 1));
+            }
+
+            foreach (string kortti in pakka)
+            {
+                Console.WriteLine(kortti);
+            }
+        }
+    }
+}
